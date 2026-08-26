00395df75e688f71dbf27b12d356d6f78fd39f6c
Ayrik Nabirahni <ayriknabirahni01@gmail.com>
2026-08-25 19:47:34 -0700

    chore: simplify constants

--- config/constants.py
+++ config/constants.py
@@ -9,8 +9,8 @@ import scipy.constants
 constant_stefan_boltzmann = scipy.constants.Stefan_Boltzmann
 constant_gravitational = scipy.constants.gravitational_constant
 # CONVERSION CONSTANTS
-constant_m_TO_AU = (1/149597870700) # from IAU 2015 Resolution B3
 constant_AU_TO_m = 149597870700 # from IAU 2015 Resolution B3
+constant_m_TO_AU = (1/constant_AU_TO_m) # from IAU 2015 Resolution B3
 constant_d_TO_s = 86400 # derived from (1day*24hours*60minutes*60seconds)
 constant_solarmass_TO_kg = 1988400e24 # from NASA GSFC Sun Fact Sheet
 constant_solarluminosity_TO_W = 3.828e26 # from IAU 2015 Resolution B3
